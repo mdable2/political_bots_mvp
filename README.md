f3dc43e7988cfd7ce025dd94adcc22583a51d66e
Mark Dabler <markrdabler@gmail.com>
2022-12-09 19:11:07 -0600

    time to do data engineering

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ As part of the AssemblyAI Hackathon 2022, this project will serve as an MVP for 
 * [12/9/22 12:40PM] Working on cloning [GODEL](https://github.com/microsoft/GODEL) to start setting up the training pipeline. Also will start work on creating a scraper to get the data from [factba](https://factba.se/). I checked `robots.txt` and it doesn't say I can't scrape so I'm going to use this as my data source since they have transcripts for Joe Biden & Donald Trump.
 
 * [12/9/22 3:14PM] Got `GODEL` cloned and the scraper written. Scraping now. Going to start on wireframing the front end now. And need to write a second scraper.
+
+* [12/9/22 7:09PM] Things have finished scraping. So now I gotta do some data engineering to get it in the right format for `GODEL` to train on. Also starting the piping of the actual app.
--- scraper/get_urls.py
+++ scraper/get_urls.py
@@ -6,47 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import pandas as pd
 
-import pickle
 from urllib.parse import urljoin
 import common.logging as logging
 import time
 import datetime
 from datetime import timedelta
-from typing import List, Set
-
-class TranscriptObject():
-    keywords: Set[str]
-    text: str
-    speaker: str
-
-    def __init__(self, keywords, text, speaker) -> None:
-        self.keywords = keywords
-        self.text = text
-        self.speaker = speaker
-
-    def __repr__(self) -> str:
-        return f"TranscriptObject()\nkeywords: {self.keywords}\nspeaker: {self.speaker}\ntext: {self.text}\n"
-    
-    def __str__(self) -> str:
-        return f"keywords: {self.keywords}\nspeaker: {self.speaker}\ntext: {self.text}\n"
-
-class TranscriptPage():
-    title: str
-    keywords: Set[str]
-    speakers: Set[str]
-    transcript_objects: List[TranscriptObject]
-
-    def __init__(self, title, keywords, speakers, transcript_objects) -> None:
-        self.title = title
-        self.keywords = keywords
-        self.speakers = speakers
-        self.transcript_objects = transcript_objects
-    
-    def __repr__(self) -> str:
-        return f"TranscriptPage()\ntitle: {self.title}\nkeywords: {self.keywords}\nspeakers: {self.speakers}\ntranscript_objects: {self.transcript_objects}\n"
-    
-    def __str__(self) -> str:
-        return f"title: {self.title}\nkeywords: {self.keywords}\nspeakers: {self.speakers}\ntranscript_objects: {self.transcript_objects}\n"     
+from typing import List
 
 
 if __name__ == "__main__":
@@ -63,44 +28,129 @@ if __name__ == "__main__":
 
         screen_height = driver.execute_script("return window.screen.height;")
         i = 1
-        df = pd.read_csv("/app/common/biden_urls.csv")
-        list_of_urls = list(df["0"])
-        transcript_pages = []
 
-        for url in list_of_urls:
-            try:
-                kws = set()
-                speakers = set()
-                logging.log(f"At url: {url}")
-                driver.get(url)
-                soup = BeautifulSoup(driver.page_source, "html.parser")
-                title = soup.find("h1", {"class": "topic-page-header transcript-header"}).text
-                results_block = soup.find('div', id="resultsblock")
-                if not results_block:
-                    logging.log(f"Skipping {url}")
-                    continue
-                transcript_objects = []
-                for result in results_block.find_all("div", {"class": "media topic-media-row mediahover"}):
-                    speaker = result.find("div", {"class": "speaker-label"}).text
-                    text = result.find("div", {"class": "transcript-text-block"}).text
-                    keywords = []
-                    for kw in result.find_all("div", {"class": "tag-keyword keyword-tooltip"}):
-                        keywords.append(kw.text.strip())
-                    for kw in result.find_all("div", {"class": "tag-entity entity-tooltip"}):
-                        keywords.append(kw.text.strip())
-                    for kw in result.find_all("div", {"class": "tag-people people-tooltip"}):
-                        keywords.append(kw.text.strip())
-                    for kw in keywords:
-                        kws.add(kw)
-                    speakers.add(speaker)
+        driver.get("https://factba.se/trump/transcripts/")
 
-                    transcript_object = TranscriptObject(set(keywords), text, speaker)
-                    transcript_objects.append(transcript_object)
-                transcript_pages.append(TranscriptPage(title, kws, speakers, transcript_objects))
-            except Exception as e:
-                logging.error(e)
-                continue
-        pickle.dump(transcript_pages, open("/app/common/biden_transcripts.pkl", "wb"))
-        logging.log("All done!")
+        logging.log("At trump endpoint")
+
+        time.sleep(3)
+
+        while True:
+            logging.log(f"Scrolling {i}")
+
+            # scroll one screen height each time
+            driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+            i += 1
+            time.sleep(scroll_pause_time)
+            # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+            scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+            # Break the loop when the height we need to scroll to is larger than the total scroll height
+            if (screen_height) * i > scroll_height:
+                break
+            
+            if i > 2300:
+                break
+        
+        logging.log("Collecting urls")
+        urls = []
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        for a in soup.find_all('a', href=True):
+            link = a["href"]
+            base = r"https://factba.se/"
+            url = urljoin(base, link)
+            urls.append(url)
+
+        logging.log("Writing to df")
+        df = pd.DataFrame(list(set(urls)))
+        df.to_csv("/app/common/trump_urls.csv")
+
+        logging.log("Done!")
+
+
+        # login_elements = driver.find_elements(
+        #     By.XPATH, "//div[text()='Login/Sign Up']")
+
+        # WebDriverWait(driver, 20).until(
+        #     EC.element_to_be_clickable(login_elements[0])).click()
+
+        # time.sleep(3)
+
+        # logging.log("About to login")
+
+        # sign_in_username = driver.find_elements(By.ID, "signInFormUsername")[1]
+        # sign_in_password = driver.find_elements(By.ID, "signInFormPassword")[1]
+        # sign_in_submit = driver.find_elements(By.NAME, "signInSubmitButton")[1]
+
+        # sign_in_username.send_keys(secrets.CHATTERQUANT_USERNAME)
+        # sign_in_password.send_keys(secrets.CHATTERQUANT_PASSWORD)
+        # WebDriverWait(driver, 20).until(
+        #     EC.element_to_be_clickable(sign_in_submit)).click()
+
+        # logging.log("Logging in")
+
+        # time.sleep(3)
+
+        # driver.get("https://chatterquant.com/popout/alerts")
+
+        # logging.log("At popout alerts")
+
+        # time.sleep(3)
+
+        # overall_alert_objects = []
+        # first_run = True
+
+        # while True:
+        #     divs = driver.find_elements(By.XPATH, "//div")
+
+        #     next_one = False
+        #     alerts_are_in_here = []
+        #     for div in divs:
+        #         if next_one:
+        #             alerts_are_in_here.append(div)
+        #         if "STOCKS ONLY" in div.text:
+        #             next_one = True
+
+        #     logging.log("Found alerts")
+
+        #     next_one = False
+        #     alerts = []
+        #     alerts_class_number = 0
+        #     for div in alerts_are_in_here:
+        #         if next_one:
+        #             if alerts_class_number > 0:
+        #                 if alerts_class_number == get_class_int(div.get_attribute("class")):
+        #                     alerts.append(div)
+        #             else:
+        #                 alerts_class_number = get_class_int(
+        #                     div.get_attribute("class")) + 1
+        #         elif "MuiSelect-root MuiSelect-select" in div.get_attribute("class"):
+        #             next_one = True
+
+        #     alert_objects = []
+        #     for alert in alerts:
+        #         p_tags = alert.find_elements(By.TAG_NAME, "p")
+        #         span_tags = alert.find_elements(By.TAG_NAME, "span")
+        #         alert_object = create_alert_object(
+        #             p_tags[0].text, p_tags[1].text, span_tags[-1].text)
+        #         alert_objects.append(alert_object)
+        #         if first_run:
+        #             overall_alert_objects.append(alert_object)
+
+        #     if not first_run:
+        #         for alert_obj in alert_objects:
+        #             if not is_repeated_alert(alert_obj, overall_alert_objects):
+        #                 print("NEW! ", alert_obj.symbol, alert_obj.alert_type,
+        #                     alert_obj.company_name, alert_obj.alert_datetime)
+        #                 # TODO: Add to db
+        #                 overall_alert_objects.append(alert_obj)
+        #     else:
+        #         first_run = False
+        #         for alert_obj in alert_objects:
+        #             print("FIRST RUN! ", alert_obj.symbol, alert_obj.alert_type,
+        #                 alert_obj.company_name, alert_obj.alert_datetime)
+        #         # TODO: Add to db
+
+        #     logging.log("Waiting 60 seconds to scrape again...")
+        #     time.sleep(60)
     except Exception as e:
         logging.error(e)
